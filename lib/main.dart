0f5ea52fc2e8466388f3bc5e3658b6e3b32d151e
Maiikuu <131999444+Maiikuu@users.noreply.github.com>
2025-12-18 20:14:31 +0700

    initialize
    initialize supabase, buat supabase

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:project/app.dart';
 import 'package:project/firebase/firebase_options.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await Supabase.initialize(
+    url: 'https://zbshxvuidxtrcbxzqzvh.supabase.co',
+    anonKey: 'sb_publishable_zgkdiW0BLTznnrx_sYaNgg_dJ2YnYix',
+  );
+
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
       .then((value) => runApp(App()));
 }
